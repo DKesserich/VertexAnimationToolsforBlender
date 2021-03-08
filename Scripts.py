34451284cc63bdb525953a685baaba49cbcc69cf
DKesserich <daffydlejeune42@gmail.com>
2021-03-08 10:52:58 -0800

    Update Scripts.py

--- Scripts.py
+++ Scripts.py
@@ -1,15 +1,17 @@
 #assign vertex offsets to texture
 vertexoffset = []
 vertexnormal = []
-for i in range(1, C.scene.frame_end):
+for i in range(1, C.scene.frame_end+1):
     C.scene.frame_set(i)
     depsgraph = C.evaluated_depsgraph_get()
     eval = C.active_object.evaluated_get(depsgraph)
     evalMesh = eval.to_mesh()
     for v in evalMesh.vertices:
-        vertexoffset.append((v.co.x - basemesh.data.vertices[v.index].co.x)*100)
-        vertexoffset.append((v.co.y - basemesh.data.vertices[v.index].co.y)*-100)
-        vertexoffset.append((v.co.z - basemesh.data.vertices[v.index].co.z)*100)
+        #worldVertex = C.active_object.matrix_world @ v.co
+        #baseWorldVert = basemesh.matrix_world @ basemesh.data.vertices[v.index].co
+        vertexoffset.append((v.co.x - basemesh.data.vertices[v.index].co.x))# - basemesh.data.vertices[v.index].co.x))
+        vertexoffset.append((v.co.y - basemesh.data.vertices[v.index].co.y)* -1)# - basemesh.data.vertices[v.index].co.z))
+        vertexoffset.append((v.co.z - basemesh.data.vertices[v.index].co.z))# - basemesh.data.vertices[v.index].co.y)*-1)
         vertexnormal.append(v.normal.x)
         vertexnormal.append(v.normal.y)
         vertexnormal.append(v.normal.z)
@@ -18,11 +20,11 @@ for i in range(1, C.scene.frame_end):
     eval.to_mesh_clear()
     
 D.images['Untitled'].pixels = vertexoffset
-	
+    
 
-#Make UVs for the offset texture.
+#Make UVs for the offset texture. Math is not right here.
 for v in C.active_object.data.vertices:
-	for loop in C.active_object.data.loops:
-		if loop.vertex_index == v.index:
-			print(Vector(((v.index+0.5)/len(C.active_object.data.vertices),0.5)))
-			C.active_object.data.uv_layers.active.data[loop.index].uv = Vector(((v.index+0.5)/len(C.active_object.data.vertices),0.5))
+    for loop in C.active_object.data.loops:
+        if loop.vertex_index == v.index:
+            print(Vector(((v.index+0.5)/len(C.active_object.data.vertices),0.5)))
+            C.active_object.data.uv_layers.active.data[loop.index].uv = Vector(((v.index+0.5)/len(C.active_object.data.vertices),0.5))
